ebcc767be583ae9d3d57e34c653e0469722a1b66
Owen <owenpickard89@gmail.com>
2022-11-08 09:48:24 -0500

    Add new functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 import time
 from webdriver_manager.firefox import GeckoDriverManager
 # import os
 import unittest
+
+MAX_WAIT = 10
+
+
 
 class NewVisitorTest(LiveServerTestCase):
     
@@ -15,7 +20,21 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(.5)
+
+
+    def test_can_start_a_list_for_one_user(self):
         # Edit has aheard about a cool new online to-do app. 
         # She goes to check out its homepage
         self.browser.get(self.live_server_url)
@@ -35,21 +54,17 @@ class NewVisitorTest(LiveServerTestCase):
 
         # when she hit enter, the page updates, and now the page lists "#1` Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-            # f"New to-do item did not appear in table. Contents were:\n {table.text}"
-
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item She enters "Use peacock featehrs to make a fly" 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+       
 
         # the page updates, and now shoes both items on her list
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
@@ -59,17 +74,51 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
         # Edit wonders whether the site will remember her listThen she sees that the site has gerenrated a quniue URL for her and there is some explanatory text to that effect
-        self.fail('Finish the Test!')
+        # self.fail('Finish the Test!')
 
         # She visits taht url and her to-do list is still there  
 
         # Satisfied, she goes back to sleep
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edit starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-# geckodriver = os.environ['geckodriver'] 
-# browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+        # She notices that the list has as unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
+        # Now a new user, Francis, comes along to the site
 
+        # We use a new browser session to make sure that no information
+        # Of Edith's is coming through from cookies
+        self.browser.quit()
+        self.browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
-# She notices the page title and header mention to-do lists
-# assert 'To-Do' in browser.title, 'Browser title was' + browser.title
+        # Francis visits the home page, there is no sign of edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He is less interesting than Edit
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy Milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unqiue URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy Milk', page_text)
+
+        # Satisfied, they both go back to sleep
