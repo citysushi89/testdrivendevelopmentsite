bbba8abfd5f3e304743b9165abcd5a464d273ebf
Owen <owenpickard89@gmail.com>
2022-11-09 20:04:14 -0500

    fabfile adjustments

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -17,9 +17,9 @@ def deploy():
 
 def _get_latest_source():
     if exists('.git'):
-        run ('git fetch')
+        run('git fetch')
     else:
-        run(f'git clone {REPO_URL}')
+        run(f'git clone {REPO_URL} .')
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f'git reset --hard {current_commit}')
 
