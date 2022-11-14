b94fc2c8c02ed808ff8c6c59ed5eb3bcc040ff6f
Owen <owenpickard89@gmail.com>
2022-11-14 10:05:20 -0500

    use form in all views, back to working state

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
-# from forms import EMPTY_ITEM_ERROR
 
 class ItemFormTest(TestCase):
 
@@ -8,7 +7,6 @@ class ItemFormTest(TestCase):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
-
 
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
