5017029ef000afc3284f4faaca13d95c475d11cf
Owen <owenpickard89@gmail.com>
2022-11-14 11:04:09 -0500

    Implement duplicate item validation at model layer

--- lists/forms.py
+++ lists/forms.py
@@ -5,12 +5,6 @@ from lists.models import Item
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
-    # item_text = forms.CharField(
-    #     widget=forms.fields.TextInput(attrs={
-    #         'placeholder': 'Enter a to-do item',
-    #         'class': 'form-control input-lg',
-    #     }),
-    # )
 
     class Meta:
         model = Item
@@ -25,3 +19,7 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.models import Item, List
 
 class ItemFormTest(TestCase):
 
@@ -19,3 +20,11 @@ class ItemFormTest(TestCase):
     def test_home_page_uses_item_form(self):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
